d34f9165860c3d4bb07aa005b84dcf79d94a959a
Sven Willner <sven.willner@gmail.com>
2017-09-22 14:25:55 +0200

    include CMakeParseArguments

--- helpers.cmake
+++ helpers.cmake
@@ -14,7 +14,7 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 set(HELPER_MODULES_PATH ${CMAKE_CURRENT_LIST_DIR})
-
+include(CMakeParseArguments)
 
 function(set_advanced_cpp_warnings TARGET)
   if(ARGN GREATER 1)
